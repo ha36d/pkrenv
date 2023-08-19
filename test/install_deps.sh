7b182e47c4c2f189fd6a3d893908eb11d800ff4b
Hamed Faramarzi <hamed.faramarzi@gmail.com>
2023-08-19 12:32:41 +0200

    fix: add rev function in pkrenv-resolve-version
    Signed-off-by: Hamed Faramarzi <hamed.faramarzi@gmail.com>

--- test/install_deps.sh
+++ test/install_deps.sh
@@ -5,14 +5,3 @@ if [[ $(uname) == 'Darwin' ]] && [ $(which brew) ]; then
   brew install grep;
 fi;
 
-if [[ "$(uname)" == *"MINGW64"* ]] || [[ "$(uname)" == *"MSYSNT"* ]] || [[ "$(uname)" == *"CYGWINNT"* ]] ; then
-function rev() {
-  (
-    copy=$1
-    len=${#copy}
-    for((i=$len-1;i>=0;i--)); do rev="$rev${copy:$i:1}"; done
-    echo $rev
-  );
-};
-export -f rev;
-fi;
