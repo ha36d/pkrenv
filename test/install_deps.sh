d672a3e2e87c7059e0b187f5c82f24d6297be4d7
Hamed Faramarzi <hamed.faramarzi@gmail.com>
2023-08-19 10:39:15 +0200

    fix: add rev function for windows
    Signed-off-by: Hamed Faramarzi <hamed.faramarzi@gmail.com>

--- test/install_deps.sh
+++ test/install_deps.sh
@@ -4,3 +4,15 @@ set -uo pipefail;
 if [[ $(uname) == 'Darwin' ]] && [ $(which brew) ]; then
   brew install grep;
 fi;
+
+if [[ "$(uname)" == *"MINGW64"* ]] || [[ "$(uname)" == *"MSYSNT"* ]] || [[ "$(uname)" == *"CYGWINNT"* ]] ; then
+function rev() {
+  (
+    copy=$1
+    len=${#copy}
+    for((i=$len-1;i>=0;i--)); do rev="$rev${copy:$i:1}"; done
+    echo $rev
+  );
+};
+export -f rev;
+fi;
